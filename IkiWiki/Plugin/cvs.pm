85844b8494dd5e032d06afd623c25b4bb4a72578
Amitai Schlair <schmonz@magnetic-babysitter.(none)>
2009-08-30 21:03:18 -0400

    whitespace

--- IkiWiki/Plugin/cvs.pm
+++ IkiWiki/Plugin/cvs.pm
@@ -303,7 +303,7 @@ sub rcs_recentchanges($) {
 	system("env TZ=UTC cvsps -q --cvs-direct -z 30 -x >$tmpfile");
 	if ($? == -1) {
 		error "couldn't run cvsps: $!\n";
-	} elsif (($? >>8) != 0) {
+	} elsif (($? >> 8) != 0) {
 		error "cvsps exited " . ($? >> 8) . ": $!\n";
 	}
 
