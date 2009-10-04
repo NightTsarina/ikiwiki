7982c3c1773feef0d7f248ff3efe09dd2cd94ad8
Joey Hess <joey@gnu.kitenet.net>
2009-10-04 16:16:50 -0400

    postsparkline and progress: also contentless dependencies

--- IkiWiki/Plugin/postsparkline.pm
+++ IkiWiki/Plugin/postsparkline.pm
@@ -48,7 +48,7 @@ sub preprocess (@) {
 		error gettext("unknown formula");
 	}
 
-	add_depends($params{page}, $params{pages});
+	add_depends($params{page}, $params{pages}, content => 0);
 
 	my @list=sort { $params{timehash}->{$b} <=> $params{timehash}->{$a} } 
 		pagespec_match_list(
