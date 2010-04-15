NightTsarina/ikiwiki

commit b7351daacd0d4a041a51b43d99b7bf589de54f53

conditional: use yesno

IkiWiki/Plugin/conditional.pm
@@ -29,7 +29,7 @@ sub preprocess_if (@) {
 	}
 
 	my $result=0;
-	if ((exists $params{all} && lc $params{all} eq "no") ||
+	if (! IkiWiki::yesno($params{all}) ||
 		# An optimisation to avoid needless looping over every page
 		# and adding of dependencies for simple uses of some of the
 		# tests.

IkiWiki.pm
@@ -1792,7 +1792,7 @@ sub add_depends ($$;$) {
 	#		$depends_simple{$page}{lc $i} |= $i{$i};
 	#	}
 	#}
-	print STDERR "warning: use of add_depends; influences not tracked\n";
+	print STDERR "warning: use of add_depends by ".caller()."; influences not tracked\n";
 
 	$depends{$page}{$pagespec} |= $deptype;
 	return 1;

t/yesno.t
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
-use Test::More tests => 10;
+use Test::More tests => 11;
 
 BEGIN { use_ok("IkiWiki"); }
 
@@ -19,3 +19,5 @@ ok(IkiWiki::yesno("NO") == 0);
 ok(IkiWiki::yesno("1") == 1);
 ok(IkiWiki::yesno("0") == 0);
 ok(IkiWiki::yesno("mooooooooooo") == 0);
+
+ok(IkiWiki::yesno(undef) == 0);

commit 50d08bc2aea1d8d52e07ae1750e73b722bd2db24

conditional: Fix bug that forced "all" mode off by default.
Commit b7351daacd0d4a041a51b43d99b7bf589de54f53 introduced the bug.

IkiWiki/Plugin/conditional.pm
@@ -30,7 +30,7 @@ sub preprocess_if (@) {
 	}
 
 	my $result=0;
-	if (! IkiWiki::yesno($params{all}) ||
+	if ((exists $params{all} && ! IkiWiki::yesno($params{all})) ||
 	    # An optimisation to avoid needless looping over every page
 	    # for simple uses of some of the tests.
 	    $params{test} =~ /^([\s\!()]*((enabled|sourcepage|destpage|included)\([^)]*\)|(and|or))[\s\!()]*)+$/) {