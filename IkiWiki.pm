673d6c958050b7ce07d5d8512d5370f9531225cd
Joey Hess <joey@kitenet.net>
2010-04-21 19:45:56 -0400

    add missing undef guard in derel

--- IkiWiki.pm
+++ IkiWiki.pm
@@ -2245,7 +2245,7 @@ sub derel ($$) {
 	if ($path =~ m!^\./!) {
 		$from=~s#/?[^/]+$## if defined $from;
 		$path=~s#^\./##;
-		$path="$from/$path" if length $from;
+		$path="$from/$path" if defined $from && length $from;
 	}
 
 	return $path;
