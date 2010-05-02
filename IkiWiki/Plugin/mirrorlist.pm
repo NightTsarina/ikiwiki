932fc0c25ffa1da6c6eb5d398752372ba7f80165
Joey Hess <joey@kitenet.net>
2010-05-02 14:41:20 -0400

    use a div, not a p .. and in html5, a nav

--- IkiWiki/Plugin/mirrorlist.pm
+++ IkiWiki/Plugin/mirrorlist.pm
@@ -40,7 +40,7 @@ sub pagetemplate (@) {
 
 sub mirrorlist ($) {
 	my $page=shift;
-	return "<p>".
+	return ($config{html5} ? '<nav id="mirrorlist">' : '<div>').
 		(keys %{$config{mirrorlist}} > 1 ? gettext("Mirrors") : gettext("Mirror")).
 		": ".
 		join(", ",
@@ -50,7 +50,7 @@ sub mirrorlist ($) {
 				qq{">$_</a>}
 			} keys %{$config{mirrorlist}}
 		).
-		"</p>";
+		($config{html5} ? '</nav>' : '</div>');
 }
 
 1
