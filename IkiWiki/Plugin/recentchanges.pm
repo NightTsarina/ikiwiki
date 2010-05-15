3dd98a3b3f66555ca3d83ec6940a9cd11cf8bec7
Joey Hess <joey@kitenet.net>
2010-05-14 20:38:08 -0400

    put back recentchangesurl
    On second thought, misctemplate can use pagetemplate hooks to provide
    it, so it's better to keep back-compat, and allow full customisation
    of how it's displayed via the template.

--- IkiWiki/Plugin/recentchanges.pm
+++ IkiWiki/Plugin/recentchanges.pm
@@ -11,8 +11,6 @@ sub import {
 	hook(type => "getsetup", id => "recentchanges", call => \&getsetup);
 	hook(type => "checkconfig", id => "recentchanges", call => \&checkconfig);
 	hook(type => "refresh", id => "recentchanges", call => \&refresh);
-	hook(type => "pageactions", id => "recentchanges", call => \&pageactions,
-		first => 1);
 	hook(type => "pagetemplate", id => "recentchanges", call => \&pagetemplate);
 	hook(type => "htmlize", id => "_change", call => \&htmlize);
 	# Load goto to fix up links from recentchanges
@@ -62,20 +60,7 @@ sub refresh ($) {
 	}
 }
 
-# Enable the recentchanges link on wiki pages.
-sub pageactions (@) {
-	my %params=@_;
-	my $page=$params{page};
-
-	if (defined $config{recentchangespage} && $config{rcs} &&
-	    $page ne $config{recentchangespage}) {
-		return htmllink($page, $page, $config{recentchangespage},
-			linktext => gettext("RecentChanges"));
-	}
-}
-
-# Backwards compatability for templates still using
-# RECENTCHANGESURL.
+# Enable the recentchanges link.
 sub pagetemplate (@) {
 	my %params=@_;
 	my $template=$params{template};
