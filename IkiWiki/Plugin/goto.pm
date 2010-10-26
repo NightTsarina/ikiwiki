ac6b5c12fa9c834555fde4a4cf06db7b792556d4
Joey Hess <joey@kitenet.net>
2010-10-25 23:31:41 -0400

    squash undef

--- IkiWiki/Plugin/goto.pm
+++ IkiWiki/Plugin/goto.pm
@@ -52,7 +52,7 @@ sub cgi_goto ($;$) {
 		IkiWiki::redirect($q, $pagestate{$page}{meta}{permalink});
 	}
 
-	if (! length $link) {
+	if (! defined $link || ! length $link) {
 		IkiWiki::cgi_custom_failure(
 			$q,
 			"404 Not Found",
