4dbb8120f760d9009f0c2639f2ccb9808150aed5
Joey Hess <joey@kitenet.net>
2011-01-05 16:08:21 -0400

    Export three cgi env vars needed for CGI->url to work.

--- IkiWiki/Wrapper.pm
+++ IkiWiki/Wrapper.pm
@@ -49,6 +49,7 @@ sub gen_wrapper () {
 	push @envsave, qw{REMOTE_ADDR QUERY_STRING REQUEST_METHOD REQUEST_URI
 	               CONTENT_TYPE CONTENT_LENGTH GATEWAY_INTERFACE
 		       HTTP_COOKIE REMOTE_USER HTTPS REDIRECT_STATUS
+		       HTTP_HOST SERVER_PORT HTTPS
 		       REDIRECT_URL} if $config{cgi};
 	my $envsave="";
 	foreach my $var (@envsave) {
