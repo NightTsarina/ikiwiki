c11b1226cae155cbffc4c198470a9b59083e8c3e
Joey Hess <joey@kitenet.net>
2011-01-05 15:43:03 -0400

    oops

--- IkiWiki.pm
+++ IkiWiki.pm
@@ -1931,7 +1931,7 @@ sub misctemplate ($$;@) {
 		title => $title,
 		wikiname => $config{wikiname},
 		content => $content,
-		baseurl => $config{url},
+		baseurl => $config{url}.'/',
 		html5 => $config{html5},
 		%params,
 	);
