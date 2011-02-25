ecd2153a70882a5a058c20af7e752d656203e13f
Joey Hess <joey@kitenet.net>
2011-02-25 17:32:36 -0400

    avoid uninitialized value warning when running -dumpsetup
    Here wikistatedir has not been configured.

--- IkiWiki/Plugin/transient.pm
+++ IkiWiki/Plugin/transient.pm
@@ -25,10 +25,12 @@ sub getsetup () {
 our $transientdir;
 
 sub checkconfig () {
-	$transientdir = $config{wikistatedir}."/transient";
-	# add_underlay treats relative underlays as relative to the installed
-	# location, not the cwd. That's not what we want here.
-	IkiWiki::add_literal_underlay($transientdir);
+	if (defined $config{wikistatedir}) {
+		$transientdir = $config{wikistatedir}."/transient";
+		# add_underlay treats relative underlays as relative to the installed
+		# location, not the cwd. That's not what we want here.
+		IkiWiki::add_literal_underlay($transientdir);
+	}
 }
 
 sub change (@) {
