690840698921e1a17831ff6dc15d3ae304abaf37
Joey Hess <joey@kitenet.net>
2011-03-28 11:41:13 -0400

    Revert "comment: Don't show comments of subpages on parent pages. (Fixes bug introduced in version 3.20100505.)"
    This reverts commit b34d31142b9fed28ec9cf77fe0c5d9f405d48c84.
    
    This was the wrong approach. It broke inlining of comment(*) on eg, a
    toplevel comment page.

--- IkiWiki/Plugin/comments.pm
+++ IkiWiki/Plugin/comments.pm
@@ -957,8 +957,7 @@ sub match_comment ($$;@) {
 		}
 	}
 
-	return match_glob($page, "$glob/*", internal => 1, @_) &&
-		! match_glob($page, "$glob/*/*", internal => 1, @_);
+	return match_glob($page, "$glob/*", internal => 1, @_);
 }
 
 sub match_comment_pending ($$;@) {
@@ -973,8 +972,7 @@ sub match_comment_pending ($$;@) {
 		return IkiWiki::FailReason->new("$page is not a pending comment");
 	}
 
-	return match_glob($page, "$glob/*", internal => 1, @_) &&
-		! match_glob($page, "$glob/*/*", internal => 1, @_);
+	return match_glob($page, "$glob/*", internal => 1, @_);
 }
 
 1
