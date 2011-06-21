886890b82d727385f5bb167ef7684288e98a5218
Joey Hess <joey@kitenet.net>
2011-06-21 15:12:02 -0400

    move headinganchors out of contrib

---
+++ IkiWiki/Plugin/headinganchors.pm
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+# quick HTML heading id adder by Paul Wise
+package IkiWiki::Plugin::headinganchors;
+
+use warnings;
+use strict;
+use IkiWiki 2.00;
+
+sub import {
+	hook(type => "sanitize", id => "headinganchors", call => \&headinganchors);
+}
+
+sub text_to_anchor {
+	my $str = shift;
+	$str =~ s/^\s+//;
+	$str =~ s/\s+$//;
+	$str = lc($str);
+	$str =~ s/[&\?"\'\.,\(\)!]//mig;
+	$str =~ s/[^a-z]/_/mig;
+	return $str;
+}
+
+sub headinganchors (@) {
+	my %params=@_;
+	my $content=$params{content};
+	$content=~s{<h([0-9])>([^>]*)</h([0-9])>}{'<h'.$1.' id="'.text_to_anchor($2).'">'.$2.'</h'.$3.'>'}gie;
+	return $content;
+}
+
+1
