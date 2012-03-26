88e8f0ced96c65c7a04f95eb5aa72679937593c9
Joey Hess <joey@kitenet.net>
2012-03-26 11:05:03 -0400

    simplify

--- IkiWiki/Plugin/meta.pm
+++ IkiWiki/Plugin/meta.pm
@@ -275,18 +275,12 @@ sub preprocess (@) {
 		push @{$metaheaders{$page}}, '<meta name="robots"'.
 			' content="'.encode_entities($value).'" />';
 	}
-	elsif ($key eq 'author') {
-		push @{$metaheaders{$page}}, '<meta name="'.
-			encode_entities($key).
-			'" content="'.encode_entities($value).'" />';
-	}
-	elsif ($key eq 'description') {
-		push @{$metaheaders{$page}}, '<meta name="'.
-			encode_entities($key).
+	elsif ($key eq 'description' || $key eq 'author') {
+		push @{$metaheaders{$page}}, '<meta name="'.$key.
 			'" content="'.encode_entities($value).'" />';
 	}
 	elsif ($key eq 'name') {
-		push @{$metaheaders{$page}}, scrub('<meta '.$key.'="'.
+		push @{$metaheaders{$page}}, scrub('<meta name="'.
 			encode_entities($value).
 			join(' ', map { "$_=\"$params{$_}\"" } keys %params).
 			' />', $page, $destpage);
