f16de0c4e3c6e9a9d8f0758bf0b78f94c0993a4e
Joey Hess <joey@kitenet.net>
2012-03-28 19:04:44 -0400

    fix hook name

--- IkiWiki/Render.pm
+++ IkiWiki/Render.pm
@@ -832,7 +832,7 @@ sub refresh () {
 		run_hooks(rendered => sub { shift->(keys %rendered) });
 		run_hooks(change => sub { shift->(keys %rendered) }); # back-compat
 	}
-	run_hooks(difference => sub {
+	run_hooks(changes => sub {
 		shift->(@$new, @$changed, @$del,
 			@$internal_new, @$internal_changed, @$internal_del);
 	});
