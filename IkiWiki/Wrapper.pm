bc99b3174cb131a86bc86853d6fb8820021d26f4
Joey Hess <joey@kitenet.net>
2014-01-02 12:24:26 -0400

    add void prototype

--- IkiWiki/Wrapper.pm
+++ IkiWiki/Wrapper.pm
@@ -182,7 +182,7 @@ void addenv(char *var, char *val) {
 	newenviron[i++]=s;
 }
 
-set_cgilock_fd (int lockfd) {
+void set_cgilock_fd (int lockfd) {
 	char *fd_s=malloc(8);
 	sprintf(fd_s, "%i", lockfd);
 	setenv("IKIWIKI_CGILOCK_FD", fd_s, 1);
